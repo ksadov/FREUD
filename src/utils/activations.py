94c3b9d6cb00dcd2059a74e16dad170c4f9f0b1f
K Sadov <ksadov@gmail.com>
2024-10-09 17:01:32 -0500

    clean up gui server code

--- src/utils/activations.py
+++ src/utils/activations.py
@@ -6,27 +6,28 @@ from src.utils.constants import SAMPLE_RATE, TIMESTEP_S
 from src.dataset.activations import MemoryMappedActivationsDataset, FlyActivationDataloader
 from tqdm import tqdm
 
-def init_map(layer_name: str, config: dict, split: str, files_to_search: int) -> torch.Tensor:
-    data_dir = config['out_folder']
-    dset = MemoryMappedActivationsDataset(data_dir, layer_name, files_to_search)
-    print("dset len", len(dset))
-    return dset
-
-
 def trim_activation(audio_fname: str, activation: torch.Tensor) -> torch.Tensor:
+    """
+    Trim the activation tensor to match the duration of the audio file
+    """
     audio_duration = torchaudio.info(audio_fname).num_frames / SAMPLE_RATE
     n_frames = int(audio_duration / TIMESTEP_S)
     return activation[:n_frames]
 
+def mmapped_top(activation_audios: MemoryMappedActivationsDataset, neuron_idx: int, n_files: int, 
+                         max_val: Optional[float]) -> list:
+    """
+    Given a memory-mapped activation dataset, return the n files that activate a given neuron the most, or the top n 
+    files under a given maximum activation value
 
-def get_top_activating_files(activation_audio_map: dict, n_files: int, neuron_idx: int) -> list[str]:
-    top_files = top_activating_files(activation_audio_map, n_files, neuron_idx)
-    return [x[1] for x in top_files[:n_files]]
-
-
-def top_activating_files(activation_audios: MemoryMappedActivationsDataset, n_files: int, neuron_idx: int, max_val: Optional[float]) -> list:
+    :param activation_audios: MemoryMappedActivationsDataset
+    :param neuron_idx: index of the neuron to search for
+    :param n_files: number of files to return
+    :param max_val: maximum activation value to consider, or None to find the files with the highest activations
+    """
     top = []
-    for activation, audio_file in activation_audios:
+    print("Searching activations...")
+    for activation, audio_file in tqdm(activation_audios):
         activation_at_idx = activation.transpose(0, 1)[neuron_idx]
         trimmed_activation = trim_activation(audio_file, activation_at_idx)
         max_activation_value = trimmed_activation.max().item()
@@ -35,29 +36,20 @@ def top_activating_files(activation_audios: MemoryMappedActivationsDataset, n_fi
             max_activation_time = max_activation_loc * TIMESTEP_S
             top.append((audio_file, trimmed_activation, max_activation_value, max_activation_time))
             top.sort(key=lambda x: x[2], reverse=True)
-    return top
-
-def search_activations(batch_folder, neuron_idx, n_files, max_val):
-    # activation map may be too big to load all at once
-    # so we just dynamically load the activations for the batch
-    # and return the top n files
-    print("Searching activations...")
-    print(f"Neuron index: {neuron_idx}")
-    print(f"N files: {n_files}")
-    top = []
-    for batch_file in os.listdir(batch_folder):
-        batch_path = os.path.join(batch_folder, batch_file)
-        batch = torch.load(batch_path)
-        top_batch_files = top_activating_files(batch, n_files, neuron_idx, max_val)
-        if len(top) < n_files:
-            top.extend(top_batch_files)
-        else:
-            top.extend(top_batch_files)
-            top.sort(key=lambda x: x[2], reverse=True)
             top = top[:n_files]
+    print("Search complete.")
     return top
 
-def get_top_fly(dataloader: FlyActivationDataloader, neuron_idx: int, n_files: int, max_val: Optional[float]) -> list:
+def fly_top(dataloader: FlyActivationDataloader, neuron_idx: int, n_files: int, max_val: Optional[float]) -> list:
+    """
+    Given a dataloader that generates activations on the fly, return the n files that activate a given neuron the most, 
+    or the top n files under a given maximum activation value
+
+    :param dataloader: FlyActivationDataloader
+    :param neuron_idx: index of the neuron to search for
+    :param n_files: number of files to return
+    :param max_val: maximum activation value to consider, or None to find the files with the highest activations
+    """
     print("Searching activations...")
     pq = []
     for act_batch, audio_files in tqdm(dataloader):
@@ -74,25 +66,30 @@ def get_top_fly(dataloader: FlyActivationDataloader, neuron_idx: int, n_files: i
     print("Search complete.")
     return pq
 
-def make_top_fn(config: dict, layer_name: str, split: str, from_disk: bool, files_to_search: Optional[int]) -> callable:
+def make_top_fn(config: dict, from_disk: bool, files_to_search: Optional[int]) -> callable:
     if from_disk:
-        activation_audio_map = init_map(layer_name, config, split, files_to_search)
-        return lambda neuron_idx, n_files, max_val: top_activating_files(
-            activation_audio_map, n_files, neuron_idx, max_val)
+        activation_audio_map = MemoryMappedActivationsDataset(
+            config['out_folder'],
+            config['layer_to_cache'],
+            max_size=files_to_search
+        )
+        return lambda neuron_idx, n_files, max_val: mmapped_top(
+            activation_audio_map, neuron_idx, n_files, max_val
+        )
     else:
         fly_dataloader = FlyActivationDataloader(
             config['data_path'],
             config['whisper_model'],
             config['sae_model'],
-            layer_name,
+            config['layer_to_cache'],
             config['device'],
-            split,
-            batch_size=50,
+            config['batch_size'],
             dl_max_workers=4,
             subset_size=files_to_search
         )
-        return lambda neuron_idx, n_files, max_val: get_top_fly(
-            fly_dataloader, neuron_idx, n_files, max_val)
+        return lambda neuron_idx, n_files, max_val: fly_top(
+            fly_dataloader, neuron_idx, n_files, max_val
+        )
         
 def get_top_activations(top_fn: callable,
                         neuron_idx: int,
--- tutorial.md
+++ tutorial.md
@@ -3,4 +3,6 @@
 2. Install the rest of the dependencies: `pip install -r requirements.txt`
 3. Download the speech dataset: `python -m src.scripts.download_audio_datasets`
 4. Collect activations from the speech dataset: `python -m src.scripts.collect_activations --config whisper-interp/src/configs/features/block_2_mlp_1.json`
-5. View the collected activations 
+- if you're low on disk space, set `collect_max` or omit the collection step altogether
+5. View the collected activations: `python -m src.scripts.gui_server --config whisper-interp/src/configs/features/block_2_mlp_1.json --from_disk`
+- exclude the `--from_disk` flag to search activations generated on-the-fly
