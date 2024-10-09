2c72dc4a6623218509a7d609abcd3c29db37ef78
K Sadov <ksadov@gmail.com>
2024-10-09 13:01:43 -0500

    improve audio dataset DL

--- src/download_audio_datsets.py
+++ src/download_audio_datsets.py
@@ -18,37 +18,23 @@ files = {
       "balanced_train_segments.csv",
       "bal_train00.tar",
       "bal_train01.tar",
-      "bal_train02.tar",
-      "bal_train03.tar",
-      "bal_train04.tar",
-      "bal_train05.tar",
-      "bal_train06.tar",
-      "bal_train07.tar",
-      "bal_train08.tar",
-      "bal_train09.tar",
       "eval_segments.csv",
-      "eval_00.tar",
-      "eval_01.tar",
-      "eval_02.tar",
-      "eval_03.tar",
-      "eval_04.tar",
-      "eval_05.tar",
-      "eval_06.tar",
-      "eval_07.tar",
-      "eval_08.tar",
+      "eval00.tar",
       "ontology.json",
   ]
 }
 
 def download_files(output_dir: str, dataset: str):
     os.makedirs(output_dir, exist_ok=True)
-    for file in tqdm(files[dataset]):
+    filtered_files = [file for file in files[dataset] if not os.path.exists(os.path.join(output_dir, file))]
+    for file in tqdm(filtered_files):
         url = os.path.join(roots[dataset], file)
         output_file = os.path.join(output_dir, file)
         if not os.path.exists(output_file):
             r = requests.get(url, stream=True)
+            r.raise_for_status()
             with open(output_file, 'wb') as f:
-                for chunk in tqdm(r.iter_content(chunk_size=1024), desc=file, unit="bytes"):
+                for chunk in tqdm(r.iter_content(chunk_size=1024), desc=file, unit="KB"):
                     if chunk:
                         f.write(chunk)
     print("All files downloaded to", output_dir)
@@ -70,10 +56,11 @@ def main():
     parser.add_argument("--output_dir", type=str, default="audio_data")
     parser.add_argument("--dataset", type=str, default="librispeech")
     args = parser.parse_args()
+    out_dir = os.path.join(args.output_dir, args.dataset)
     if args.dataset not in roots:
         raise ValueError(f"Dataset {args.dataset} not found in {roots.keys()}")
-    download_files(args.output_dir, args.dataset)
-    extract_files(args.output_dir)
+    download_files(out_dir, args.dataset)
+    extract_files(out_dir)
 
 if __name__ == "__main__":
     main()
